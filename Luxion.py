dbf4254161ec0bd1995004b7cd38174c1011742f
Morten Kristensen <msk@nullpointer.dk>
2018-03-19 21:31:34 +0100

    Luxion C++ include guard variant

--- Luxion.py
+++ Luxion.py
@@ -1,6 +1,7 @@
 import sublime_plugin
+from datetime import datetime
 
-# NOTE: Run `sublime.log_commands(True)` while testing to see what's going on.
+from .utils import guard_path_to_root
 
 WRAP_LINE = "// " + "*" * 97 + "\n"
 
@@ -18,3 +19,18 @@ class WrapLuxionFunctionCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     if not self.view.is_read_only() and self.view.size() > 0:
       self.wrap_regions(self.view, edit)
+
+class LuxionInsertCppIncludeGuardCommand(sublime_plugin.TextCommand):
+  """Inserts Luxion C++ include guard."""
+
+  def run(self, edit):
+    if not self.view.is_read_only():
+      self.__insert_guard(edit)
+
+  def __insert_guard(self, edit):
+    guard = guard_path_to_root(self.view.file_name())
+    year = datetime.today().year
+    txt = "// (c) Copyright 2003-{} Luxion ApS - All Rights Reserved\n#ifndef {}\n#define {}\n\n".\
+      format(year, guard, guard)
+    self.view.insert(edit, 0, txt)
+    self.view.insert(edit, self.view.size(), "\n\n#endif // {}\n".format(guard))
